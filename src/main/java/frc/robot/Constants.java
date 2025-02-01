09faff4d48f8a0bc6039400b2b5f88c6f75d33e5
KavEn06 <kavinnimal@gmail.com>
2025-02-01 18:21:27 -0500

    indexer subsystem, just spins motor atp

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,8 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+  public static class IndexConstants {
+    public static int kIndexMotorCAN = 0;
+  }
 }
+
