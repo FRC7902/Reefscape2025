9122c4474cb946cae2f98a67058fbf9a77afa258
Brianna Azan <brianna.azan@gmail.com>
2025-02-09 20:53:54 -0500

    testing

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -4,11 +4,43 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IntakeConstants;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 public class IntakeSubsystem extends SubsystemBase {
+
+  private final SparkMax m_intakeMotor = new SparkMax(IntakeConstants.intakeCANid, MotorType.kBrushless);
+  private final DigitalInput intakeSensor = new DigitalInput(IntakeConstants.beamBrakePort);
+
+  private final XboxController m_operatorStick;
+  public boolean beamBrakeOverride = false;
+  
   /** Creates a new IntakeSubsystem. */
-  public IntakeSubsystem() {}
+  //testing
+  public IntakeSubsystem(XboxController operatorStick) {
+    m_operatorStick = operatorStick;
+    stopMotor();
+    m_intakeMotor.setInverted(true);
+  }
+
+  public void stopMotor() {
+    m_intakeMotor.stopMotor();
+  }
+
+  public void setPower(double power) {
+    m_intakeMotor.set(power);
+  }
+
+  public boolean getBeamBrake() {
+    return intakeSensor.get();
+  }
 
   @Override
   public void periodic() {
