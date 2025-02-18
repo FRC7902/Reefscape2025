5eacfb8784f1f2fe916e630ff700d154c333e2c9
Willie Pai <PaisWillie@gmail.com>
2025-02-17 21:10:08 -0500

    feat: move and rename elevator setpoint command

--- src/main/java/frc/robot/commands/teleop/elevator/ElevatorReefSetpointCommand.java
+++ src/main/java/frc/robot/commands/teleop/elevator/ElevatorReefSetpointCommand.java
@@ -2,18 +2,17 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.elevator;
+package frc.robot.commands.teleop.elevator;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.RobotContainer;
-import frc.robot.Constants.ElevatorConstants;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class ElevatorReefSetpoint extends Command {
+public class ElevatorReefSetpointCommand extends Command {
   private double m_targetHeight;
 
   /** Creates a new ElevatorSetpoint. */
-  public ElevatorReefSetpoint(double targetHeight) {
+  public ElevatorReefSetpointCommand(double targetHeight) {
     m_targetHeight = targetHeight;
     addRequirements(RobotContainer.m_elevatorSubsystem);
   }
@@ -33,7 +32,7 @@ public class ElevatorReefSetpoint extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-   // RobotContainer.m_elevatorSubsystem.stop();
+    // RobotContainer.m_elevatorSubsystem.stop();
   }
 
   // Returns true when the command should end.
