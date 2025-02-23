917f98141d1d37f0f1252b17fada84d9da6bdccf
Willie Pai <PaisWillie@gmail.com>
2025-02-23 18:13:16 -0500

    fix: linting

--- src/main/java/frc/robot/commands/teleop/climb/InitiateClimbCommand.java
+++ src/main/java/frc/robot/commands/teleop/climb/InitiateClimbCommand.java
@@ -5,7 +5,6 @@
 package frc.robot.commands.teleop.climb;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.RobotContainer;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
