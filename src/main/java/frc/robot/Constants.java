4dac7db44fe6e2584e313c6150c739728a28402d
PaisWillie-FireBirds <wpai@markhamfirebirds.ca>
2025-03-01 17:04:47 -0500

    feat: lower coral outtake speed (to prevent coral from bouncing out)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -80,7 +80,7 @@ public final class Constants {
         public static int kIndexMotorCAN = 35;
         public static final double kRampRate = 0;
 
-        public static final double kOuttakePower = 1;
+        public static final double kOuttakePower = 0.75;
         public static final double kIntakePower = 1;
         public static final double kCorrectionPower = 0.2;
 
