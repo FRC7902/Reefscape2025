7fc66330ecf626067005a9eb3b2e07d1be2bfd02
Vishva Sritharan <66765929+vishva12124@users.noreply.github.com>
2025-03-05 12:14:11 -0500

    used static references of subsystem instead of creating separate objects for them in aligntoreef.java

--- src/main/java/frc/robot/commands/teleop/visions/AlignToReef.java
+++ src/main/java/frc/robot/commands/teleop/visions/AlignToReef.java
@@ -5,47 +5,26 @@
 package frc.robot.commands.teleop.visions;
 
 import java.util.function.DoubleSupplier;
-
-import com.google.flatbuffers.Constants;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.RobotContainer;
-import frc.robot.Constants.CoralIndexerConstants;
-import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.visions.CameraInterface;
 import swervelib.SwerveInputStream;
-import frc.robot.subsystems.CoralIndexerSubsystem;
-import frc.robot.subsystems.SwerveSubsystem;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class AlignToReef extends Command {
-  /** Creates a new InitiateClimbCommand. */
-  private SwerveSubsystem drivebase;
-  private CameraInterface autoAlignCam;
-  private CommandXboxController m_driverController;
-  private RobotContainer m_robotContainer;
-  private CoralIndexerSubsystem m_coralIndexerSubsystem;
+  /** Creates a new AlignToReefCommand. */
   private boolean endCommand = false;
 
-  public AlignToReef(SwerveSubsystem drivebase, CameraInterface autoAlignCam, CommandXboxController m_driverController, RobotContainer m_robotContainer, CoralIndexerSubsystem m_coralIndexerSubsystem) {
+  public AlignToReef() {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements();
-    this.drivebase = drivebase;
-    this.autoAlignCam = autoAlignCam;
-    this.m_driverController = m_driverController;
-    this.m_robotContainer = m_robotContainer;
-    this.m_coralIndexerSubsystem = m_coralIndexerSubsystem;
   }
 
   public SwerveInputStream getDriveAngularVelocity() {
-    return SwerveInputStream.of(drivebase.getSwerveDrive(), 
-    () -> m_driverController.getLeftY() * -1,
-    () -> m_driverController.getLeftX() * -1)
-    .withControllerRotationAxis(() -> m_driverController.getRightX() * -1)
+    return SwerveInputStream.of(RobotContainer.drivebase.getSwerveDrive(), 
+    () -> RobotContainer.m_driverController.getLeftY() * -1,
+    () -> RobotContainer.m_driverController.getLeftX() * -1)
+    .withControllerRotationAxis(() -> RobotContainer.m_driverController.getRightX() * -1)
     .deadband(OperatorConstants.DEADBAND)
     .scaleTranslation(0.8)
     .allianceRelativeControl(true);        
@@ -55,23 +34,23 @@ public class AlignToReef extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    autoAlignCam.camera.getAllUnreadResults();
-    autoAlignCam.resetTargetDetector();
-    endCommand = !m_coralIndexerSubsystem.hasCoral();
+    RobotContainer.m_autoAlignCam.camera.getAllUnreadResults();
+    RobotContainer.m_autoAlignCam.resetTargetDetector();
+    endCommand = !RobotContainer.m_indexSubsystem.hasCoral();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (!(autoAlignCam.cameraSawTarget())) {
-      autoAlignCam.getCameraResults();
-      Commands.run(() -> drivebase.driveFieldOriented(getDriveAngularVelocity())); 
+    if (!(RobotContainer.m_autoAlignCam.cameraSawTarget())) {
+      RobotContainer.m_autoAlignCam.getCameraResults();
+      RobotContainer.drivebase.driveFieldOriented(getDriveAngularVelocity()); 
     }
-    else if (autoAlignCam.cameraSawTarget()) {
-      DoubleSupplier xTrans = () -> autoAlignCam.poseFromRobotToTag.getX();
-      DoubleSupplier yTrans = () -> autoAlignCam.poseFromRobotToTag.getY();
-      DoubleSupplier maxAngularRotation = () -> drivebase.getMaximumChassisAngularVelocity();
-      Commands.run(() -> drivebase.driveCommand(xTrans, yTrans, maxAngularRotation));
+    else if (RobotContainer.m_autoAlignCam.cameraSawTarget()) {
+      DoubleSupplier xTrans = () -> RobotContainer.m_autoAlignCam.poseFromRobotToTag.getX();
+      DoubleSupplier yTrans = () -> RobotContainer.m_autoAlignCam.poseFromRobotToTag.getY();
+      DoubleSupplier maxAngularRotation = () -> RobotContainer.drivebase.getMaximumChassisAngularVelocity();
+      RobotContainer.drivebase.driveCommand(xTrans, yTrans, maxAngularRotation);
     }
   }
 
