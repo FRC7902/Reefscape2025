4d518549dcc96806991def5155e968acf0076bc0
Willie Pai <PaisWillie@gmail.com>
2025-03-22 01:04:13 -0400

    fix: linting

--- src/main/java/frc/robot/commands/teleop/coral_indexer/ManualIntakeCoralCommand.java
+++ src/main/java/frc/robot/commands/teleop/coral_indexer/ManualIntakeCoralCommand.java
@@ -7,9 +7,13 @@ package frc.robot.commands.teleop.coral_indexer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.RobotContainer;
 
-/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+/*
+ * You should consider using the more terse Command factories API instead
+ * https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#
+ * defining-commands
+ */
 public class ManualIntakeCoralCommand extends Command {
-  private double m_power;
+    private double m_power;
 
     /** Creates a new IntakeCoralCommand. */
     public ManualIntakeCoralCommand(double power) {
@@ -18,26 +22,25 @@ public class ManualIntakeCoralCommand extends Command {
         m_power = power;
     }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {}
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    RobotContainer.m_indexSubsystem.setPower(m_power);
-  }
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        RobotContainer.m_indexSubsystem.setPower(m_power);
+    }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    RobotContainer.m_indexSubsystem.stop();
-  }
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        RobotContainer.m_indexSubsystem.stop();
+    }
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    // return RobotContainer.m_indexSubsystem.hasCoral();
-    return RobotContainer.m_indexSubsystem.isDeepBeamBroken();
-  }
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return RobotContainer.m_indexSubsystem.isDeepBeamBroken();
+    }
 }
